17a005b014a8acacf182eab1e88855ca3828b734
Bjarki Snær Magnússon <bjarkim@ru.is>
2019-09-05 14:04:09 +0000

    Added instruction for sequence.py

---
+++ sequence.py
@@ -0,0 +1,5 @@
+# 1. Get how long the sequence is from the user
+# 2. implement a(1) = and a(2) = 2
+# 3. Implement the sequence a(n) = a(n-1) + a(n-2) + a(n-3)
+# 4. print each number of the sequence with a "," inbetween
+
