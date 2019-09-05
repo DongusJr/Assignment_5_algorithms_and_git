f7f6f99d809f7b26efe4ec21776d51205fab4439
Bjarki Snær Magnússon <bjarkim@ru.is>
2019-09-05 14:27:54 +0000

    algorithm added

--- sequence.py
+++ sequence.py
@@ -1,6 +1,15 @@
 # 1. Get how long the sequence is from the user
-# 2. implement a(1) = and a(2) = 2
-# 3. Implement the sequence a(n) = a(n-1) + a(n-2) + a(n-3)
+# 2. Realise that the sequence doesn't work for a(0),a(1) and a(2), so let a(0) = 1, a(1) = 2 and a(2) = 3
+# 3. Implement the sequence a(n) = a(n-1)*2 - (n-3)
 # 4. print each number of the sequence with a "," inbetween
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+seq_num = 0
+difference = 0
+for i in range(0, n):
+    difference = seq_num*2 - (i-3)
+    if 0 <= i <=2:
+        seq_num += 1
+    else:
+        seq_num = difference
+    print(seq_num, end=", ")
