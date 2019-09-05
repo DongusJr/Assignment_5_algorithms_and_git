b0c5657fc85c5379b08996bca3e54cc0ba0cff0e
Bjarki Snær Magnússon <bjarkim@ru.is>
2019-09-05 13:42:23 +0000

    Added the default text from Mimir classroom

--- max_int.py
+++ max_int.py
@@ -2,3 +2,8 @@
 # 2. Check if the number is a positive integer
 # 3. Run a while loop while its an positive integer
 # 4. Check if the input is bigger than the max
+# 5. Break out of the loop and print the max number if the user typed in a negitive number
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
