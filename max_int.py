e8ff85e6e975e1222506b1534c0c7d9815985f86
Bjarki Snær Magnússon <bjarkim@ru.is>
2019-09-05 13:41:03 +0000

    Added instructions

---
+++ max_int.py
@@ -0,0 +1,4 @@
+# 1. Ask for an input from the user
+# 2. Check if the number is a positive integer
+# 3. Run a while loop while its an positive integer
+# 4. Check if the input is bigger than the max
